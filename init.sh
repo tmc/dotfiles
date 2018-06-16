40a9cc8f2a41bb24a1028176c608f98928c65028
Travis Cline <travis.cline@gmail.com>
2018-06-15 21:20:36 -0700

    update

--- init.sh
+++ init.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 # curl https://raw.githubusercontent.com/tmc/dotfiles/master/init.sh | bash -x
-test -f .bashrc && cp .bashrc .bashrc-bak
-test -f .vimrc && cp .vimrc .vimrc-bak
+test -f .bashrc && cp .bashrc .bashrc-$(date +%s)
+test -f .vimrc && cp .vimrc .vimrc-$(date +%s)
+test -f .gitconfig && cp .gitconfig .gitconfig-$(date +%s)
 curl -o .bashrc https://raw.githubusercontent.com/tmc/dotfiles/master/.bashrc
 curl -o .vimrc https://raw.githubusercontent.com/tmc/dotfiles/master/.vimrc
+curl -o .gitconfig https://raw.githubusercontent.com/tmc/dotfiles/master/.gitconfig
