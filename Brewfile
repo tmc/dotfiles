6cbe44359043c84be3e3ce4ab99c933f6f8e5be3
Travis Cline <travis.cline@gmail.com>
2020-12-21 16:29:16 -0800

    mbpa

---
+++ Brewfile
@@ -0,0 +1,19 @@
+brew "mas"
+cask "iterm2"
+#cask "google-chrome"
+#mas "1Password", id: 443987910
+# This takes a long time to install
+#mas "XCode", id: 497799835
+
+brew "bash-completion"
+brew "gh"
+brew "git"
+brew "go"
+brew "postgresql"
+brew "python@3.9"
+brew "readline"
+brew "ruby"
+brew "rust"
+brew "vim"
+brew "wget"
+brew "shellcheck"
---
+++ macinit.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# macinit - initialize a new mac machine.
+set -eu
+GITROOTP="${HOME}/go/src/github.com/${USER}"
+GITROOT="${GITROOTP}/dotfiles"
+FILES=(.bashrc .vimrc .gitconfig .iterm2 .sh_aliases)
+file -d "${GITROOTP}" || mkdir -p "${GITROOTP}"
+file -d "${GITROOT}" || (cd "${GITROOTP}"; git clone https://github.com/tmc/dotfiles.git)
+cd "${HOME}"
+for f in "${FILES[@]}"; do 
+    test -L "${f}" && mv "${f}" "${f}-$(date +%s))"
+    ln -s "${GITROOT}/${f}" .
+done
+
+# Ensure brew is installed.
+command -v brew > /dev/null || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install homebrew bundle.
+cd "${GITROOT}"
+brew bundle install
